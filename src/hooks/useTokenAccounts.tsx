caecfb6d9bc69acbf7a9ff1466d5939ad8b39fcf
DR497 <47689875+dr497@users.noreply.github.com>
2022-04-24 16:57:50 +0800

    fix native SOL + refresh token balances faster

--- src/hooks/useTokenAccounts.tsx
+++ src/hooks/useTokenAccounts.tsx
@@ -1,6 +1,12 @@
 import { useWallet, useConnection } from "@solana/wallet-adapter-react";
 import { useRequest } from "ahooks";
-import { TOKEN_PROGRAM_ID, AccountLayout, RawAccount } from "@solana/spl-token";
+import {
+  TOKEN_PROGRAM_ID,
+  AccountLayout,
+  RawAccount,
+  NATIVE_MINT,
+} from "@solana/spl-token";
+import { PublicKey } from "@solana/web3.js";
 
 export const useTokenAccounts = () => {
   const { connection } = useConnection();
@@ -14,14 +20,32 @@ export const useTokenAccounts = () => {
     const accounts = results.value
       .map((e) => AccountLayout.decode(e.account.data))
       .filter((e) => !!e) as RawAccount[];
+
+    // Add native SOL
+    const balances = await connection.getBalance(publicKey);
+    accounts.push({
+      mint: NATIVE_MINT,
+      owner: publicKey,
+      amount: BigInt(balances),
+      delegateOption: 0,
+      delegate: PublicKey.default,
+      state: 1,
+      isNative: BigInt(1),
+      isNativeOption: 1,
+      delegatedAmount: BigInt(0),
+      closeAuthorityOption: 0,
+      closeAuthority: PublicKey.default,
+    });
+
     accounts.sort((a, b) => {
       return parseInt(b.amount.toString()) - parseInt(a.amount.toString());
     });
+
     return accounts;
   };
 
   return useRequest(fn, {
     refreshDeps: [connected, publicKey, connection.rpcEndpoint],
-    pollingInterval: 5_000,
+    pollingInterval: 1_500,
   });
 };
